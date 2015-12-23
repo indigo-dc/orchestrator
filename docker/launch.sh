5df1423bfbff8643490c1aae62e9d8c4e2ef95a5
Alberto Brigandì <a.brigandi@reply.it>
2015-12-23 16:57:31 +0100

    Improved docker buid

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-CLUSTER_MESSAGING_PASSWORD="pwd"
+
 #IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
-$JBOSS_HOME/bin/standalone.sh -c $JBOSS_CONF_FILE -Djboss.bind.address=$HOSTNAME -Djboss.bind.address.management=$HOSTNAME -Djgroups.bind_addr=$HOSTNAME -Djboss.node.name=$HOSTNAME -Djboss.messaging.cluster.password=$CLUSTER_MESSAGING_PASSWORD
+
+java -jar /usr/share/java/saxon.jar -o:$JBOSS_HOME/standalone/configuration/$JBOSS_CONF_FILE -s:$JBOSS_HOME/standalone/configuration/$JBOSS_CONF_FILE -xsl:/datasourcesConfig.xsl \
+	orchestrator.DB.endpoint=$ORCHESTRATOR_DB_ENDPOINT \
+	orchestrator.DB.name=$ORCHESTRATOR_DB_NAME \
+	orchestrator.DB.user=$ORCHESTRATOR_DB_USER \
+	orchestrator.DB.pwd=$ORCHESTRATOR_DB_PWD
+
+CLUSTER_MESSAGING_PASSWORD="pwd"
+$JBOSS_HOME/bin/standalone.sh -c $JBOSS_CONF_FILE -Djboss.bind.address=$HOSTNAME -Djboss.bind.address.management=$HOSTNAME \
+	-Djgroups.bind_addr=$HOSTNAME -Djboss.node.name=$HOSTNAME -Djboss.messaging.cluster.password=$CLUSTER_MESSAGING_PASSWORD
