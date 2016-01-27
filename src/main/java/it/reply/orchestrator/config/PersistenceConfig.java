ba399e3df66205a773b32fa6d85a197f67595d28
Alberto Brigandì <a.brigandi@reply.it>
2016-01-27 16:20:50 +0100

    Changed transactionManager from JPA to JTA

--- src/main/java/it/reply/orchestrator/config/PersistenceConfig.java
+++ src/main/java/it/reply/orchestrator/config/PersistenceConfig.java
@@ -7,11 +7,11 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.jta.JtaTransactionManager;
 
 import java.util.Properties;
 
@@ -43,8 +43,7 @@ public class PersistenceConfig {
 
   @Bean
   public PlatformTransactionManager transactionManager() throws NamingException {
-    EntityManagerFactory factory = entityManagerFactory().getObject();
-    return new JpaTransactionManager(factory);
+    return new JtaTransactionManager();
   }
 
   /**
