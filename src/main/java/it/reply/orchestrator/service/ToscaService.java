404b4721df1b953a7233945787e9d61548d1637d
Alberto Brigandì <a.brigandi@reply.it>
2016-03-10 09:57:11 +0100

    Change ToscaService interface
    ToscaService needs to operates on ArchiveRoot instead of Topology

--- src/main/java/it/reply/orchestrator/service/ToscaService.java
+++ src/main/java/it/reply/orchestrator/service/ToscaService.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import javax.annotation.Nonnull;
 
 import alien4cloud.model.topology.Topology;
+import alien4cloud.tosca.model.ArchiveRoot;
 import alien4cloud.tosca.parser.ParsingException;
 
 public interface ToscaService {
 
-  public @Nonnull String getTemplateFromTopology(@Nonnull Topology topology);
-
-  public @Nonnull Topology getTopologyFromTemplate(@Nonnull String toscaTemplate)
+  public @Nonnull ArchiveRoot getArchiveRootFromTemplate(@Nonnull String toscaTemplate)
       throws IOException, ParsingException;
+
+  public @Nonnull String getTemplateFromTopology(@Nonnull ArchiveRoot archiveRoot)
+      throws IOException;
 
 }
