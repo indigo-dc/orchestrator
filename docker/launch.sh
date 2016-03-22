4b0a02c8727f26a95ad4ef74b1a2ef1adc809162
Alberto Brigandì <a.brigandi@reply.it>
2016-03-22 12:38:25 +0100

    Make IM properties configurable from ENV VARS

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 
@@ -11,6 +11,24 @@ java -jar /usr/share/java/saxon.jar -o:$JBOSS_HOME/standalone/configuration/$JBO
 	workflow.DB.name=$WORKFLOW_DB_NAME \
 	workflow.DB.user=$WORKFLOW_DB_USER \
 	workflow.DB.pwd=$WORKFLOW_DB_PWD
+
+IM_PROP_FILE="$JBOSS_HOME/standalone/deployments/$WAR_NAME.war/WEB-INF/classes/im-config/im-java-api.properties"
+
+if [[ $IM_URL ]];
+	then sed -i "s/^\(url=\).*$/\1${IM_URL}/" ${IM_PROP_FILE};
+fi;
+if [[ $PROXY_DIR ]];
+	then sed -i "s/^\(file\.directory=\).*$/\1${PROXY_DIR}/" ${IM_PROP_FILE};
+fi;
+if [[ $OPENNEBULA_AUTH_FILE_PATH ]];
+	then sed -i "s/^\(opennebula\.auth\.file\.path=\).*$/\1${OPENNEBULA_AUTH_FILE_PATH}/" ${IM_PROP_FILE};
+fi;
+if [[ $OPENSTACK_AUTH_FILE_PATH ]];
+	then sed -i "s/^\(openstack.auth\.file\.path=\).*$/\1${OPENSTACK_AUTH_FILE_PATH}/" ${IM_PROP_FILE};
+fi;
+if [[ $ONEDOCK_AUTH_FILE_PATH ]];
+	then sed -i "s/^\(onedock\.auth\.file\.path=\).*$/\1${ONEDOCK_AUTH_FILE_PATH}/" ${IM_PROP_FILE};
+fi;
 
 CLUSTER_MESSAGING_PASSWORD="pwd"
 $JBOSS_HOME/bin/standalone.sh -c $JBOSS_CONF_FILE -Djboss.bind.address=$HOSTNAME -Djboss.bind.address.management=$HOSTNAME \
