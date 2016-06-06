78d3ee75c75602cef055eae75ab70442e5dd1863
Lorenzo Biava <l.biava@reply.it>
2016-06-06 12:12:50 +0200

    Remove CPRanker stub

--- src/main/java/it/reply/orchestrator/service/commands/GetProvidersRank.java
+++ src/main/java/it/reply/orchestrator/service/commands/GetProvidersRank.java
@@ -3,13 +3,11 @@ package it.reply.orchestrator.service.commands;
 import it.reply.orchestrator.dto.RankCloudProvidersMessage;
 import it.reply.orchestrator.dto.ranker.CloudProviderRankerRequest;
 import it.reply.orchestrator.dto.ranker.Monitoring;
-import it.reply.orchestrator.dto.ranker.RankedCloudProvider;
 import it.reply.orchestrator.service.CloudProviderRankerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,16 +33,9 @@ public class GetProvidersRank extends BaseRankCloudProvidersCommand {
             .withSla(rankCloudProvidersMessage.getSlamPreferences().getSla())
             .withMonitoring(monitoring);
 
-    // FIXME Remove once the ranker is available
     // Get provider rank and save in message
-    try {
-      rankCloudProvidersMessage
-          .setRankedCloudProviders(cloudProviderRankerService.getProviderRanking(cprr));
-    } catch (Exception e2) {
-      rankCloudProvidersMessage.setRankedCloudProviders(
-          Arrays.asList(new RankedCloudProvider("Name1", 1, true, ""), new RankedCloudProvider(
-              "STUB PROVIDER NAME -> Remove once the ranker is available", 0, true, "Error msg")));
-    }
+    rankCloudProvidersMessage
+        .setRankedCloudProviders(cloudProviderRankerService.getProviderRanking(cprr));
 
     return rankCloudProvidersMessage;
   }
