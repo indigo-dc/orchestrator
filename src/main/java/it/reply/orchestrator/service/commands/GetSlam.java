78b1f2e5cbb6fc53591f85658f1e4bf0388d9aba
Alberto Brigandì <a.brigandi@reply.it>
2016-09-15 13:13:01 +0200

    Support multiple SLAs for cloud providers
    Fixes #110

--- src/main/java/it/reply/orchestrator/service/commands/GetSlam.java
+++ src/main/java/it/reply/orchestrator/service/commands/GetSlam.java
@@ -16,15 +16,18 @@ public class GetSlam extends BaseRankCloudProvidersCommand {
   private SlamService slamService;
 
   @Override
-  protected RankCloudProvidersMessage
-      customExecute(RankCloudProvidersMessage rankCloudProvidersMessage) {
+  protected RankCloudProvidersMessage customExecute(
+      RankCloudProvidersMessage rankCloudProvidersMessage) {
     rankCloudProvidersMessage.setSlamPreferences(slamService.getCustomerPreferences());
 
     // Get VO (customer) preferences and SLAs (infer available Cloud Providers from it)
     for (Sla sla : rankCloudProvidersMessage.getSlamPreferences().getSla()) {
       // Create Cloud Provider, add to the list
-      CloudProvider cp = new CloudProvider(sla.getProvider());
-      rankCloudProvidersMessage.getCloudProviders().put(sla.getProvider(), cp);
+      CloudProvider cp = rankCloudProvidersMessage.getCloudProviders().get(sla.getProvider());
+      if (cp == null) {
+        cp = new CloudProvider(sla.getProvider());
+        rankCloudProvidersMessage.getCloudProviders().put(sla.getProvider(), cp);
+      }
 
       // Get provider's services
       for (Service service : sla.getServices()) {
