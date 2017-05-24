d871132ab1d848cbb969b905de7c71e054007103
Alberto Brigandì <a.brigandi@reply.it>
2017-05-24 16:20:27 +0200

    Improve TemplateService

--- src/test/java/it/reply/orchestrator/service/TemplateServiceTest.java
+++ src/test/java/it/reply/orchestrator/service/TemplateServiceTest.java
@@ -16,48 +16,48 @@
 
 package it.reply.orchestrator.service;
 
-import java.util.UUID;
+import it.reply.orchestrator.controller.ControllerTestUtils;
+import it.reply.orchestrator.dal.entity.Deployment;
+import it.reply.orchestrator.exception.http.NotFoundException;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import it.reply.orchestrator.controller.ControllerTestUtils;
-import it.reply.orchestrator.dal.entity.Deployment;
-import it.reply.orchestrator.dal.repository.DeploymentRepository;
-import it.reply.orchestrator.exception.http.NotFoundException;
+
+import java.util.UUID;
 
 public class TemplateServiceTest {
 
-  @InjectMocks
-  private TemplateServiceImpl templateServiceImpl = new TemplateServiceImpl();
+  private TemplateService templateService;
 
   @Mock
-  private DeploymentRepository deploymentRepository;
+  private DeploymentService deploymentService;
 
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
+    templateService = new TemplateServiceImpl(deploymentService);
   }
 
   @Test
   public void getTemplate() {
-    Deployment createDeployment = ControllerTestUtils.createDeployment();
-    String id = UUID.randomUUID().toString();
-    Mockito.when(deploymentRepository.findOne(id)).thenReturn(createDeployment);
+    Deployment deployment = ControllerTestUtils.createDeployment();
+    String deploymentId = deployment.getId();
 
-    Assert.assertEquals(templateServiceImpl.getTemplate(id), createDeployment.getTemplate());
+    Mockito.when(deploymentService.getDeployment(deploymentId)).thenReturn(deployment);
+
+    Assert.assertEquals(templateService.getTemplate(deploymentId), deployment.getTemplate());
   }
-
 
   @Test(expected = NotFoundException.class)
   public void failGetTemplate() {
-    String id = UUID.randomUUID().toString();
-    Mockito.when(deploymentRepository.findOne(id)).thenReturn(null);
-    templateServiceImpl.getTemplate(id);
+    String deploymentId = UUID.randomUUID().toString();
+    Mockito.when(deploymentService.getDeployment(deploymentId))
+        .thenThrow(new NotFoundException(deploymentId));
+    templateService.getTemplate(deploymentId);
   }
 
 }
