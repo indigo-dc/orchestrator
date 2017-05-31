f8629d89fc9a521fdd5cbec08e4fad5046724012
Alberto Brigandì <a.brigandi@reply.it>
2017-05-31 11:43:04 +0200

    Optimize JPA entities
     - Use Sets instead of Lists for *ToMany relationships
     - Use proper entity relationships to represent resource
    interdependencies

--- src/test/java/it/reply/orchestrator/service/DeploymentServiceTest.java
+++ src/test/java/it/reply/orchestrator/service/DeploymentServiceTest.java
@@ -46,6 +46,7 @@ import it.reply.orchestrator.service.security.OAuth2TokenService;
 import it.reply.workflowmanager.orchestrator.bpm.BusinessProcessManager;
 import it.reply.workflowmanager.orchestrator.bpm.BusinessProcessManager.RUNTIME_STRATEGY;
 
+import org.assertj.core.api.Assertions;
 import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
 import org.junit.Assert;
 import org.junit.Before;
@@ -200,20 +201,17 @@ public class DeploymentServiceTest {
 
     Assert.assertEquals(returneDeployment.getResources().size(), 2);
 
-    Assert.assertThat(returneDeployment.getResources().get(0).getToscaNodeName(),
-        anyOf(is(nodeName1), is(nodeName2)));
-    Assert.assertEquals(returneDeployment.getResources().get(0).getToscaNodeType(), nodeType);
-    Assert.assertEquals(returneDeployment.getResources().get(0).getState(), NodeStates.INITIAL);
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(0));
-
-    Assert.assertThat(returneDeployment.getResources().get(1).getToscaNodeName(),
-        anyOf(is(nodeName1), is(nodeName2)));
-    Assert.assertEquals(returneDeployment.getResources().get(1).getToscaNodeType(), nodeType);
-    Assert.assertEquals(returneDeployment.getResources().get(1).getState(), NodeStates.INITIAL);
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(1));
+    Assertions.assertThat(returneDeployment.getResources())
+        .extracting(Resource::getToscaNodeName)
+        .containsExactlyInAnyOrder(nodeName1, nodeName2);
+    Assertions.assertThat(returneDeployment.getResources()).allSatisfy(resource -> {
+      Assertions.assertThat(resource.getToscaNodeType()).isEqualTo(nodeType);
+      Assertions.assertThat(resource.getState()).isEqualTo(NodeStates.INITIAL);
+    });
+    
+    returneDeployment.getResources().forEach(resource -> Mockito.verify(resourceRepository).save(resource));
 
     Mockito.verify(deploymentRepository, Mockito.atLeast(1)).save(returneDeployment);
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(0));
   }
 
   @Test
@@ -265,12 +263,12 @@ public class DeploymentServiceTest {
     Deployment returneDeployment = basecreateDeploymentSuccessful(deploymentRequest, nts);
 
     Assert.assertEquals(returneDeployment.getResources().size(), 2);
-    Assert.assertEquals(returneDeployment.getResources().get(0).getToscaNodeName(), nodeName);
-    Assert.assertEquals(returneDeployment.getResources().get(0).getToscaNodeType(), nodeType);
-    Assert.assertEquals(returneDeployment.getResources().get(1).getToscaNodeName(), nodeName);
-    Assert.assertEquals(returneDeployment.getResources().get(1).getToscaNodeType(), nodeType);
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(0));
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(1));
+    Assertions.assertThat(returneDeployment.getResources()).allSatisfy(resource -> {
+      Assertions.assertThat(resource.getToscaNodeName()).isEqualTo(nodeName);
+      Assertions.assertThat(resource.getToscaNodeType()).isEqualTo(nodeType);
+    });
+    
+    returneDeployment.getResources().forEach(resource -> Mockito.verify(resourceRepository).save(resource));
 
   }
 
@@ -306,15 +304,14 @@ public class DeploymentServiceTest {
 
     Deployment returneDeployment = basecreateDeploymentSuccessful(deploymentRequest, nts);
 
-    Assert.assertEquals(returneDeployment.getResources().size(), 2);
-    Assert.assertThat(returneDeployment.getResources().get(0).getToscaNodeName(),
-        anyOf(is(nodeName1), is(nodeName2)));
-    Assert.assertEquals(returneDeployment.getResources().get(0).getToscaNodeType(), nodeType);
-    Assert.assertThat(returneDeployment.getResources().get(1).getToscaNodeName(),
-        anyOf(is(nodeName1), is(nodeName2)));
-    Assert.assertEquals(returneDeployment.getResources().get(1).getToscaNodeType(), nodeType);
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(0));
-    Mockito.verify(resourceRepository).save(returneDeployment.getResources().get(1));
+    Assertions.assertThat(returneDeployment.getResources())
+        .extracting(Resource::getToscaNodeName)
+        .containsExactlyInAnyOrder(nodeName1, nodeName2);
+    Assertions.assertThat(returneDeployment.getResources()).allSatisfy(resource -> {
+      Assertions.assertThat(resource.getToscaNodeType()).isEqualTo(nodeType);
+    });
+    
+    returneDeployment.getResources().forEach(resource -> Mockito.verify(resourceRepository).save(resource));
   }
 
   @Test(expected = NotFoundException.class)
