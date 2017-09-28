6ebb1d7f8003f6c3624d1a90e6234025cec7433c
Alberto Brigandì <a.brigandi@reply.it>
2017-09-28 17:24:22 +0200

    Organization and owner

--- src/main/java/it/reply/orchestrator/dal/repository/DeploymentRepository.java
+++ src/main/java/it/reply/orchestrator/dal/repository/DeploymentRepository.java
@@ -17,17 +17,49 @@
 package it.reply.orchestrator.dal.repository;
 
 import it.reply.orchestrator.dal.entity.Deployment;
+import it.reply.orchestrator.dal.entity.OidcEntity;
 import it.reply.orchestrator.dal.entity.OidcEntityId;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface DeploymentRepository extends PagingAndSortingRepository<Deployment, String> {
+public interface DeploymentRepository
+    extends PagingAndSortingRepository<Deployment, String> {
 
-  public Page<Deployment> findByOwner_oidcEntityId(OidcEntityId ownerId, Pageable pageable);
+  public static final String IN_SAME_ORGANIZATION =
+      "(d.owner is null "
+          + "or (d.owner.organization = ?#{#requester.organization} "
+          + "and d.owner.oidcEntityId.issuer = ?#{#requester.oidcEntityId.issuer}))";
+
+  @Query("select d "
+      + "from #{#entityName} d "
+      + "where d.owner.oidcEntityId = ?#{#ownerId}")
+  public Page<Deployment> findAllByOwner(@Param("ownerId") OidcEntityId ownerId, Pageable pageable);
+
+  @Query("select d "
+      + "from #{#entityName} d "
+      + "where d.owner.oidcEntityId = ?#{#ownerId} "
+      + "and " + IN_SAME_ORGANIZATION)
+  public Page<Deployment> findAllByOwner(@Param("requester") OidcEntity requester,
+      @Param("ownerId") OidcEntityId ownerId,
+      Pageable pageable);
+
+  @Query("select d "
+      + "from #{#entityName} d "
+      + "where d.id = ?#{#id} "
+      + "and " + IN_SAME_ORGANIZATION)
+  public Deployment findOne(@Param("requester") OidcEntity requester, @Param("id") String id);
+
+  @Query("select d "
+      + "from #{#entityName} d "
+      + "where " + IN_SAME_ORGANIZATION)
+  public Page<Deployment> findAll(@Param("requester") OidcEntity requester, Pageable pageable);
+
 }
