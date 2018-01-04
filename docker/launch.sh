c2c684c56496be6a4ad49f3476cf4ab877208aae
Alberto Brigandì <a.brigandi@reply.it>
2018-01-04 12:22:37 +0100

    Happy New Year 2018!

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2015-2017 Santer Reply S.p.A.
+# Copyright © 2015-2018 Santer Reply S.p.A.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
