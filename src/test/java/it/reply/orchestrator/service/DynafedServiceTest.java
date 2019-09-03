78fb3ae4b99e459a1ff8ea08de3ea2eb8e2db89a
Michele Perniola <michele.perniola@ba.infn.it>
2019-09-03 16:09:37 +0200

    aligned with master-vault

--- src/test/java/it/reply/orchestrator/service/DynafedServiceTest.java
+++ src/test/java/it/reply/orchestrator/service/DynafedServiceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015-2018 Santer Reply S.p.A.
+ * Copyright © 2015-2019 Santer Reply S.p.A.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,7 +95,7 @@ public class DynafedServiceTest {
   private final static String fileName = "fileName";
 
   @Before
-  public void setup() {
+  public void setup() throws Exception {
 
     when(oauth2TokenService.executeWithClientForResult(eq(oidcTokenId), any(), any()))
         .then(a -> ((ThrowingFunction) a.getArguments()[1]).apply(accessToken));
