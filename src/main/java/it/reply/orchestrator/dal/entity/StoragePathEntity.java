3c9a69b3c1f3505ffd5427de367cfc53334749f6
Andriy Lapshynov <a.lapshynov@reply.it>
2019-12-31 15:42:52 +0100

    Added post request to add a new control path.  In second moment orchestrator will control a list of path for check with incoming,  new data availability automatic notification.

---
+++ src/main/java/it/reply/orchestrator/dal/entity/StoragePathEntity.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2015-2020 Santer Reply S.p.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.reply.orchestrator.dal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(indexes = {
+    @Index(columnList = "createdAt")
+})
+@Getter
+@Setter
+@AllArgsConstructor 
+public class StoragePathEntity extends AbstractResourceEntity {
+
+  @Column(nullable = false)
+  private String storagePath;
+  
+  public StoragePathEntity() {
+    
+  }
+}
