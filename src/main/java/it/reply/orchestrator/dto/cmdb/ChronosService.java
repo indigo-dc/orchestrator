a61652cb21c439eedbeec4e89836bb522a39258c
Alberto Brigandì <alberto-brigandi@users.noreply.github.com>
2020-03-26 18:02:33 +0100

    Update src/main/java/it/reply/orchestrator/dto/cmdb/ChronosService.java

--- src/main/java/it/reply/orchestrator/dto/cmdb/ChronosService.java
+++ src/main/java/it/reply/orchestrator/dto/cmdb/ChronosService.java
@@ -48,7 +48,7 @@ public class ChronosService extends MesosFrameworkService<ChronosServiceProperti
       @NonNull String hostname,
       @Nullable String parentServiceId,
       boolean iamEnabled,
-      @Nullable boolean publicIpAssignable,
+      boolean publicIpAssignable,
       @NonNull ChronosServiceProperties properties) {
     super(id, serviceType, endpoint, providerId, type, publicService, region, hostname,
             parentServiceId, iamEnabled, publicIpAssignable, properties);
